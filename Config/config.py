3aede7ae55f9c1be83260814ba9eaa99c4416b72
Divya Rao <dsrao0712@gmail.com>
2021-11-12 10:17:26 +0530

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -1,10 +1,11 @@
 class TestData:
-
+    # Path to your chrome driver
     CHROME_EXECUTABLE_PATH = "D://chromedriver.exe"
-    BASE_URL = "https://demo-frontend.liquiloan.in/admin/login"
-    USER_NAME = "admin@liquiloans.com"
-    PASSWORD = "password@123"
-    LOGIN_PAGE_TITLE = "Liquiloans | Login"
+    # Enter the url , user name and password for logging in
+    BASE_URL = ""
+    USER_NAME = ""
+    PASSWORD = ""
+    LOGIN_PAGE_TITLE = "" 
 
 
 
